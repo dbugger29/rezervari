a2f6d16d52ebac4e6579c93b8a564df57733a5ed
dbugger29 <33966859+dbugger29@users.noreply.github.com>
2019-04-14 23:53:42 +0300

    fixed recursive alarms

--- background.js
+++ background.js
@@ -6,25 +6,41 @@ var settings = {BEFORE_TIME_REZERVATION : 10000}; // for login
 
  chrome.runtime.onInstalled.addListener(function() {
 	// to do here - maybe reload background or reset?
+	console.log('Extension installed...');
+    restart_alarm(null, true);
 });
 
-var createDate = (day, hours) =>
+chrome.runtime.onStartup.addListener(function () {
+    console.log('Extension started up...');
+    restart_alarm(null, true);
+});
+
+var createDate = (day, hours, next_week) =>
 {
 	let dayofweek =0;//none
 	switch( day.toLowerCase() )
 	{
-		case "sunday":		dayofweek += 1; 
-		case "saturday":	dayofweek += 1; 
-		case "friday":		dayofweek += 1; 
-		case "thursday":	dayofweek += 1; 
-		case "wednesday":	dayofweek += 1; 
-		case "tuesday":		dayofweek += 1; 
-		case "monday":		dayofweek += 1; 
+		case "sunday":
+		case "duminica":					dayofweek += 1; 
+		case "saturday":
+		case "sambata":						dayofweek += 1; 
+		case "friday":		
+		case "vineri":						dayofweek += 1; 
+		case "thursday":
+		case  "joi":						dayofweek += 1; 
+		case "wednesday":
+		case "miercuri":					dayofweek += 1; 
+		case "tuesday":
+		case "marti":						dayofweek += 1; 
+		case "monday":
+		case "luni":						dayofweek += 1; 
 			break;
 	}
 	dayofweek = dayofweek == 1 ? 7 : dayofweek -1;
 	let return_date = new Date();
 	return_date.setDate(return_date.getDate() + (dayofweek + 7 - return_date.getDay()) % 7);
+	if(next_week) return_date.setDate(return_date.getDate() + 7);
+	console.info(return_date);
 	let p_hour = hours.split(":");
 	let hour = parseInt(p_hour[0]);
 	let minutes = parseInt(p_hour[1]);
@@ -36,12 +52,14 @@ var createDate = (day, hours) =>
 	//to do here - 20 sec
 	return_date.setSeconds(0);
 	return_date = new Date( return_date.getTime() - settings.BEFORE_TIME_REZERVATION ); // in case of login needs
-	console.log(return_date);
+	if(return_date < new Date() && !next_week)
+			return createDate(day, hours, true);
 	return return_date;
 }
 
- var restart_alarm = (alarm_name, b_restart_all) => {
+ var restart_alarm = (alarm_name, b_restart_all, next_week) => {
 	
+	console.info("restarting alarms");
 	var set_alarms = () => {
 		chrome.storage.sync.get(["worldclass_rezervations" ], function(result) {
 			if(b_restart_all)
@@ -50,7 +68,7 @@ var createDate = (day, hours) =>
 				{
 					//if( "day" in result.worldclass_rezervations[key] )
 					//{
-						alert(JSON.stringify(result.worldclass_rezervations[key]));
+						//console.info(JSON.stringify(result.worldclass_rezervations[key]));
 						let alarm_name = result.worldclass_rezervations[key].alarm_name;
 						let alarm_time = createDate(result.worldclass_rezervations[key].day, result.worldclass_rezervations[key].time);
 						//console.info(alarm_name, alarm_time);
@@ -64,7 +82,7 @@ var createDate = (day, hours) =>
 				{
 					if(alarm_name == result.worldclass_rezervations[key].alarm_name )
 					{
-						alert(JSON.stringify(result.worldclass_rezervations[key]));
+						//console.info(JSON.stringify(result.worldclass_rezervations[key]));
 						let alarm_name = result.worldclass_rezervations[key].alarm_name;
 						let alarm_time = createDate(result.worldclass_rezervations[key].day, result.worldclass_rezervations[key].time);
 						//console.info(alarm_name, alarm_time);
@@ -89,6 +107,8 @@ var createDate = (day, hours) =>
 	else
 		if(alarm_name)
 			chrome.alarms.clear(alarm_name, b_clear_alarms);
+		else
+			b_clear_alarms();
  }
 
 chrome.storage.onChanged.addListener(function(changes, namespace) {
@@ -108,6 +128,7 @@ chrome.storage.onChanged.addListener(function(changes, namespace) {
 chrome.alarms.onAlarm.addListener(function(alarm)
 {
 	var alarm_start_date = new Date();
+	chrome.browserAction.setIcon({path: 'drink_water_red.png'});
 	chrome.tabs.create({"url":"https://members.worldclass.ro/"}, (tab) =>
 	{
 		chrome.storage.sync.set({"worldclass_current_alarm": alarm.name, "worldclass_alarm_status":-1 });
@@ -118,11 +139,8 @@ chrome.alarms.onAlarm.addListener(function(alarm)
 			chrome.tabs.create({"url":"https://members.worldclass.ro/member-schedule.php"}, (tab) =>
 			{
 				chrome.storage.sync.set({"worldclass_current_alarm": alarm.name, "worldclass_alarm_status":0 });
-				// recreate alarm
-				setTimeout( () =>
-				{
-					restart_alarm(alarm.name, false);
-				}, 60000); // peste 60 secunde
+				chrome.browserAction.setIcon({path: 'drink_water16.png'});
+				restart_alarm(alarm.name, false, true);
 			});
 		}, time_to_rezervation);
 	});
--- popup.js
+++ popup.js
@@ -2,29 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 'use strict';
-/*
-function setAlarm(event) {
-  let minutes = parseFloat(event.target.value);
-  chrome.browserAction.setBadgeText({text: 'ON'});
-  chrome.alarms.create({delayInMinutes: minutes});
-  chrome.storage.sync.set({minutes: minutes});
-  window.close();
-}
-
-function clearAlarm() {
-  chrome.browserAction.setBadgeText({text: ''});
-  chrome.alarms.clearAll();
-  window.close();
-}
-
-//An Alarm delay of less than the minimum 1 minute will fire
-// in approximately 1 minute incriments if released
-document.getElementById('sampleSecond').addEventListener('click', setAlarm);
-document.getElementById('15min').addEventListener('click', setAlarm);
-document.getElementById('30min').addEventListener('click', setAlarm);
-document.getElementById('cancelAlarm').addEventListener('click', clearAlarm);
-*/
-
 
 var ShowResetCredentials = () => {
 		document.getElementById("settings_credentials").style.display = "block";
@@ -46,6 +23,7 @@ var SetReservations = () => {
 	
 		document.getElementById("create_reservations").style.display = "block";
 }
+
 var CheckTime = (sTime) => {
 	var returnTime = sTime;
 	if( /\d\d:\d\d (AM|PM)*/g.exec(sTime) )
@@ -68,7 +46,6 @@ var AddReservations = () => {
 	
 	var e = document.getElementById("class_day");
 	var rez_day = e.options[e.selectedIndex].text;
-	//var rez_day =  (document.querySelector('input#class_name')).value;
 	chrome.storage.sync.get(["worldclass_rezervations" ], function(result) {
           //console.log('Value currently is ' + result.worldclass_rezervations);
 		  if(!result.worldclass_rezervations || typeof result.worldclass_rezervations == "undefined")
@@ -80,9 +57,7 @@ var AddReservations = () => {
 					"name"			: rez_name,
 					"alarm_name"	:rez_day + rez_time
 				});
-			// TO DO HERE -- CHECK IF REZERVATION EXISTS ALREADY
 			chrome.storage.sync.set({"worldclass_rezervations": result.worldclass_rezervations}, function() {
-					//console.info('Rezervations updated');
 					document.getElementById("create_reservations").style.display = "none";
 			});
 	});
@@ -105,10 +80,25 @@ var ResetAll = () => {
 	});
 }
 
+
+var ShowAlarms = () =>
+{
+	chrome.alarms.getAll( function(result) {
+		var s_result = "";
+		//scheduledTime
+		for(var idx=0; idx< result.length; idx++)
+		{
+			s_result += result[idx].name + "-" + new Date(result[idx].scheduledTime) + "\n";
+		}
+		alert(s_result);
+	});
+}
+
 document.getElementById('i_reset_credentials').addEventListener('click', ShowResetCredentials);
 document.getElementById('i_update_credentials').addEventListener('click', UpdateCredentials);
 document.getElementById('i_set_reservations').addEventListener('click', SetReservations);
 document.getElementById('i_add_reservation').addEventListener('click', AddReservations);
 document.getElementById('i_show_reservations').addEventListener('click', ShowReservations);
+document.getElementById('i_show_alarms').addEventListener('click', ShowAlarms);
 document.getElementById('i_reset_all').addEventListener('click', ResetAll);
 
